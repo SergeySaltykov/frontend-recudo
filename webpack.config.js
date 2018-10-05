f6a548c09dac4f657ce08d839389ab43b0cd89b5
s.saltykov <s.saltykov@dev.userstory.ru>
2018-10-05 17:01:11 +0700

    добавил минимизацию для less оы

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const path = require('path');
 const paths = {
     build: path.resolve(__dirname, './www/build'),
@@ -9,18 +10,43 @@ const paths = {
     www: path.resolve(__dirname, './www'),
 };
 const defaultPort = 8000;
-const env = process.env.NODE_ENV || 'development';
-const isProd = 'production' === env;
+const isProd = 'production' === process.env.NODE_ENV;
 
 const plugins = [
     new HtmlWebPackPlugin({
         alwaysWriteToDisk: true, // генерация записи на диск для локального запуска
-        inject: true, // все скрипты буду грузиться после body
+        inject: true, // все скрипты будут грузиться после body
         hash: true, // добавляет в конце файла хеш чтобы не кешировались стили
         template: path.resolve(paths.src, 'index.html'), // путь к шаблону
         filename: path.resolve(paths.www, './index.html'), // путь к файлу
     }),
     new HtmlWebpackHarddiskPlugin(),  // плагин позволяет убрать пути по умолчанию
+    new MiniCssExtractPlugin({
+        filename: isProd ? '[name].min.css' : '[name].css',
+        chunkFilename: isProd ? '[id].min.css' : '[id].css',
+    }),
+];
+
+/*минимизация сss и js*/
+const minimizer = [
+    new UglifyJsPlugin({
+        // не собирает билды
+        // compress: {
+        //     typeofs: false,
+        //     unsafe_Function: true,
+        //     unsafe_comps: true,
+        //     unsafe_math: true,
+        //     unsafe_proto: true,
+        // },
+        // output: {
+        //     ascii_only: true,
+        //     comments: true,
+        // },
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+    }),
+    new OptimizeCSSAssetsPlugin({}),
 ];
 
 module.exports = {
@@ -40,14 +66,28 @@ module.exports = {
                 test: /\.(js|jsx)$/,              // определяем тип файлов
                 exclude: /node_modules/,    // исключаем из обработки папку node_modules
                 use: {
-                    loader: "babel-loader"  // определяем загрузчик
+                    loader: 'babel-loader'  // определяем загрузчик
                 }
             },
             {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+            {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"] // порядок добавление загрузчиков важен
-            }
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            },
         ]
+    },
+    optimization: {
+        minimizer: minimizer,
     },
     output: {
         filename: '[name].min.js',
